161e12efa4bda0716f27b365156c904b8e8bc805
xntwpz <110713089+xntwpz@users.noreply.github.com>
2024-04-23 20:05:55 +0700

    update script

--- database/TaskBoardInsert.sql
+++ database/TaskBoardInsert.sql
@@ -1,11 +1,17 @@
 SELECT * FROM taskboard.tasks;
 
-INSERT INTO Tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn)
+INSERT INTO Tasks (taskTitle, taskDescription, taskAssignees, taskStatus) 
 VALUES 
-('Design API specs for PBI1-2', 'Need to specify endpoints, request body, response body, status', 'Pang; Pong;', 'To Do', '2024-04-19 01:00:00', '2024-04-19 01:00:00'),
-('Implement backend for authentication', 'Implement authentication logic using JWT tokens', 'John Doe', 'Doing', '2024-04-20 09:30:00', '2024-04-20 09:30:00');
+    ('Implement Login Page', 'Create HTML/CSS for the login page and integrate with backend',
+    'John', 'To Do');
+    
+UPDATE Tasks
+SET taskStatus = 'To Do'
+WHERE taskId = 1 ;
 
-INSERT INTO Tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn)
-VALUES 
-('ออกแบบสเปค API สำหรับ PBI1-2', 'ต้องระบุเอ็นด์พอยท์, ร้องขอข้อมูล, ตอบกลับข้อมูล, สถานะ', 'ป้าง; ป้อง;', 'To Do', '2024-04-19 01:00:00', '2024-04-19 01:00:00'),
-('สร้างระบบการยืนยันตัวตนด้านหลัง', 'สร้างโลจิกการยืนยันตัวตนโดยใช้โทเค็น JWT', 'John Doe', 'Doing', '2024-04-20 09:30:00', '2024-04-20 09:30:00');
+    
+    
+--     ('Design Database Schema', 'Define tables, relationships, and constraints for the database', 'Alice, Bob', 'Doing', NOW(), NOW()),
+--     ('Refactor Codebase', 'Review existing code and refactor for better performance and readability', 'Carol', 'To Do', NOW(), NOW()),
+--     ('Deploy Application to Production', 'Prepare deployment scripts and deploy the application to production servers', 'David', 'Doing', NOW(), NOW()),
+--     ('Write User Documentation', 'Create user guides and documentation for the application', 'Emily', 'To Do', NOW(), NOW());
--- database/TaskBoardTable.sql
+++ database/TaskBoardTable.sql
@@ -2,16 +2,17 @@ SHOW VARIABLES LIKE 'character_set_system';
 
 create database taskBoard ;
 use taskBoard;
+
 CREATE TABLE Tasks (
     taskID INT AUTO_INCREMENT PRIMARY KEY,
     taskTitle VARCHAR(100) CHARACTER SET utf8mb4 NOT NULL,
     taskDescription VARCHAR(500) CHARACTER SET utf8mb4,
     taskAssignees VARCHAR(30) CHARACTER SET utf8mb4,
     taskStatus ENUM('No Status', 'To Do', 'Doing', 'Done') NOT NULL DEFAULT 'No Status',
-    createdOn DATETIME NOT NULL,
-    updatedOn DATETIME NOT NULL,
+    createdOn DATETIME default current_timestamp NOT NULL,
+    updatedOn DATETIME default current_timestamp on update current_timestamp NOT NULL,
     CONSTRAINT check_taskTitle_not_empty CHECK (LENGTH(TRIM(taskTitle)) > 0),
     CONSTRAINT check_taskDescription_length CHECK (LENGTH(taskDescription) <= 500),
     CONSTRAINT check_taskAssignees_length CHECK (LENGTH(taskAssignees) <= 30)
 );
-
+DEFAULT now() ON UPDATE now()

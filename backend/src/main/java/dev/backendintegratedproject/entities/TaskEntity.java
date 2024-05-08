da28c69c80b082e41751ce7242a73407a2631f7b
Poramade Winyunawan <128454462+PrmWiny@users.noreply.github.com>
2024-05-08 22:55:15 +0700

    up to date f / d / b
    up to date f / d / b

--- backend/src/main/java/dev/backendintegratedproject/entities/TaskEntity.java
+++ backend/src/main/java/dev/backendintegratedproject/entities/TaskEntity.java
@@ -9,7 +9,7 @@ import java.util.TimeZone;
 @Getter
 @Setter
 @Entity
-@Table(name = "tasks")
+@Table(name = "Tasks")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,4 @@
-drop database taskboard;
+drop database taskBoard;
 create database taskBoard ;
 use taskBoard;
 
@@ -13,7 +13,7 @@ CREATE TABLE Tasks (
     CONSTRAINT check_taskTitle_not_empty CHECK (LENGTH(TRIM(taskTitle)) > 0)
 );
 
-INSERT INTO tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) VALUES 
+INSERT INTO Tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) VALUES 
 ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 'Assignees1Assignees2Assignees3', 'NO_STATUS', '2024-04-22 09:00:00', '2024-04-22 09:00:00'),
 ('Repository', NULL, NULL, 'TO_DO', '2024-04-22 09:05:00', '2024-04-22 14:00:00'),
 ('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', 'DOING', '2024-04-22 09:10:00', '2024-04-25 00:00:00'),

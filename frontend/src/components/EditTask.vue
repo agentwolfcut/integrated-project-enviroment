4357189f8254ce5fc7eca0bdf50b861195bbacea
xntwpz <nattawan.kump@mail.kmutt.ac.th>
2024-05-17 01:28:58 +0700

    fix edit
    bugs : createOn , updateOn , fetch value after edit

--- frontend/src/components/EditTask.vue
+++ frontend/src/components/EditTask.vue
@@ -114,7 +114,7 @@ const formatLocalDate = (dateString) => {
                                         Status</label>
                                     <select id="status" v-model="previousTask.status"
                                         class="itbkk-status bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
-                                        <option v-for="status in statusOptions" :key="status.id" :value="status.name">
+                                        <option v-for="status in statusOptions" :key="status.id" :value="status.id">
                                             {{ status.name }}
                                         </option>
                                     </select>

7482303237ee729ce6242611a6184bbf33dc4776
agentwolfcut <phurikornnm@gmail.com>
2024-08-17 22:14:37 +0700

    Create UserService

--- backend/src/main/java/dev/backendintegratedproject/controllers/UserController.java
+++ backend/src/main/java/dev/backendintegratedproject/controllers/UserController.java
@@ -1,5 +1,14 @@
 package dev.backendintegratedproject.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v2/users")
+@CrossOrigin(origins = {"http://localhost:5173",
+                        "http://ip23ft.sit.kmutt.ac.th",
+                        "http://intproj23.sit.kmutt.ac.th"})
 public class UserController {
 
 }

4529dc830f9cd0205221fe50d5d83eda19ef08ff
agentwolfcut <phurikornnm@gmail.com>
2024-08-18 00:26:12 +0700

    success UserEntity

--- backend/src/main/java/dev/backendintegratedproject/entities/UserEntity.java
+++ backend/src/main/java/dev/backendintegratedproject/entities/UserEntity.java
@@ -1,4 +1,35 @@
 package dev.backendintegratedproject.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "oid", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "email", nullable = false, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+
 }

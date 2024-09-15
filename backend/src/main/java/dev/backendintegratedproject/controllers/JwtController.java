3f66e6f5048924d09c5034d515802b3981c00104
xntwpz <110713089+xntwpz@users.noreply.github.com>
2024-09-15 18:18:56 +0700

    502 > 405 deploy

--- backend/src/main/java/dev/backendintegratedproject/controllers/JwtController.java
+++ backend/src/main/java/dev/backendintegratedproject/controllers/JwtController.java
@@ -3,13 +3,17 @@ package dev.backendintegratedproject.controllers;
 
 import dev.backendintegratedproject.securities.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "http://ip23ft.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th"})
+
 public class JwtController {
 
     @Autowired
